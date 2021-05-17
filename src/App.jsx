ecafc8c7b75580192d6c093c2363dc8d8eb4d5c2
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2021-05-18 01:31:16 +0530

    fixed a bug

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
             <Route path="/near-me" element={<NearMe />} />
           </Routes>
         </div>
-        {location.pathname !== "/"  && <MusicPlayer />}
+        { (location.pathname !== "/" && location.pathname !=="/about-us")  && <MusicPlayer />}
       </div>
     </>
   );
